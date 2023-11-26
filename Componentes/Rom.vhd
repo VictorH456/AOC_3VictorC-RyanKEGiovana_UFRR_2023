e9d2e50f349c74b44ee0630a96f950262457d540
giovanayuu <142135537+giovanayuu@users.noreply.github.com>
2023-11-25 20:11:08 -0400

    Add files via upload

--- Componentes/Rom.vhd
+++ Componentes/Rom.vhd
@@ -1,27 +1,28 @@
---Rom
---Bibliotecas e pacotes
+-- Rom
+-- Bibliotecas e pacotes
 LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
-USE IEEE.std_logic_arith.ALL;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
 
---Entidade
+-- Entidade
 entity Rom is
-	port(
-	endereco:		in std_logic_vector(15 downto 0);
-	saida:			out std_logic_vector(15 downto 0)
-	);
-end entity;	
+    port(
+        endereco : in std_logic_vector(15 downto 0);
+        saida    : out std_logic_vector(15 downto 0)
+    );
+end entity;
+
 architecture Behavior of Rom is
-	type memoria_array is array(natural range <>) of std_logic_vecto(15 downto 0);
-	constant operacoes: memoria_array(0 to 17) :=
-	
-	--Valores
-	--(0 => "1100000011000000", -- Coloca 0
-	 --1 => "1101000111010001", -- Coloca 1 
-	 --2 => "0000100000001000", -- ADD
-	 --3 => "0001000000010000", -- ADD
-	 --4 => "0110101001101010", -- bne;
-		--others => "1000000010000000");
-begin		
-	saida <= operacoes(conv_interger(unsigned(endereco)));
+    type memoria_array is array(natural range <>) of std_logic_vector(15 downto 0);
+    constant operacoes : memoria_array(0 to 17) :=
+        (
+            0 => "1100000011000000", -- Coloca 0
+            1 => "1101000111010001", -- Coloca 1
+            2 => "0000100000001000", -- ADD
+            3 => "0001000000010000", -- ADD
+            4 => "0110101001101010", -- bne
+            others => "1000000010000000"
+        );
+begin
+    saida <= operacoes(conv_integer(unsigned(endereco)));
 end architecture;
--- Componentes/Ula.vhd
+++ Componentes/Ula.vhd
@@ -1,40 +1,44 @@
 --Ula
 --Bibliotecas e pacotes
 LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
 --Entidade
 entity Ula is
 PORT(
 			Clock: 			in std_logic;
-			AluOp:			in std_logic(3 downto 0);
+			AluOp:			in std_logic_vector(3 downto 0);
 			rs,rd,rt:		in std_logic_vector(15 downto 0);
 			resultado:		out std_logic_vector(15 downto 0);
-			z, overflow:	out std_logic;
+			z, overflow:	out std_logic
 	);
 end entity;
 architecture Behavior of Ula is
-
 begin
 	process (AluOp, rs,rd,rt)
 	begin
 		case AluOp is
-		when "0000" =>--add
-			resultado <= rs + rd;
-		when "0001" =>--sub
-			resultado <= rs - rd;
-		when "0010" =>--lw
-			resultado <= rs;
-		when "0011" =>--sw
-			resultado <= rs;
-		when "0100" =>--lw
-			resultado <= rs;
-		WHEN "101" => 
-				if(rs <= rd) then
-					resultado <= "1111111111111111";
-				else 
-					resultado <= "0000000000000000";
-				end if;	
-		when others =>
-			resultado <= "0000000000000000";
+			when "0000" =>--add
+				resultado <= rs + rd;
+			when "0001" =>--sub
+				resultado <= rs - rd;
+			when "0010" =>--lw
+				resultado <= rs;
+			when "0011" =>--sw
+				resultado <= rs;
+			when "0100" =>--lw
+				resultado <= rs;
+			WHEN "101" => 
+					if(rs <= rd) then
+						resultado <= "1111111111111111";
+					else 
+						resultado <= "0000000000000000";
+					end if;	
+			when others =>
+				resultado <= "0000000000000000";
+			end case;
+		end process;
 end architecture;
